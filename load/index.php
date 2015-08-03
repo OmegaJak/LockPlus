13b6dee288ebd48a4ec3aadcef6ed8537b2ca01e
JunesiPhone <street.visions@yahoo.com>
2015-08-03 15:32:23 -0500

    Added load theme by name button

--- load/index.php
+++ load/index.php
@@ -1,15 +1,54 @@
 <html>
 <head>
   <title></title>
+  <?php
+require_once "pages/Paginated.php";
+require_once "pages/DoubleBarLayout.php";
+?>
+
   <?php include("parse.php");?>
   <link rel="stylesheet" type="text/css" href="../php/preview.css">
 </head>
 <body>
 <?php
-$dir    = '../php/themes';
 echo '<script type="text/javascript" src="func.js"></script>';
+echo '<style>.nav{position: absolute;text-align: center;top: 10px;margin: 0 auto;width: 100%;}#load{position:absolute;z-index:999;right:25px;background-color:#21b9b0; border-radius:2px;padding:5px;color:white;}</style>';
+echo '<div id="load" onclick="loadtheme()">Enter Theme</div>';
+$dir    = '../php/themes';
+
+$list = glob("$dir/*.plist");
+function mtimecmp($a, $b) {
+        $mt_a = filemtime($a);
+        $mt_b = filemtime($b);
+        if ($mt_a == $mt_b)
+            return 0;
+        else if ($mt_a < $mt_b)
+            return -1;
+        else
+            return 1;
+    }
+usort($list, "mtimecmp");
+$reversed = array_reverse($list);
+
+$page = $_GET['page'];
+$pagedResults = new Paginated($reversed, 24, $page);
+
 echo "<div id='themelist'>";
-foreach (glob("$dir/*.plist") as $filename) {
+
+
+while($row = $pagedResults->fetchPagedRow()) {  //when $row is false loop terminates
+    $path = $row;
+    $name = basename($row,'.plist');
+    $plistDocument = new DOMDocument();
+    $plistDocument->load($path);
+    $array = parsePlist($plistDocument);
+    echo '<div class="theme"><img title="'.$name.'" onclick="opentheme(this.title)" class="themeImage" src="' . $array['ThemePreview'] . '"/><span class="themeName">'.$name.'</span></div>';
+  }
+
+  $pagedResults->setLayout(new DoubleBarLayout());
+  echo $pagedResults->fetchPagedNavigation();
+
+/*foreach (glob("$dir/*.plist") as $filename) {
  //echo $filename;
  $path = $filename;
  $name = basename($filename,'.plist');
@@ -18,7 +57,7 @@ foreach (glob("$dir/*.plist") as $filename) {
  $array = parsePlist($plistDocument);
  //echo '<div class="themelist" title="'.$name.'">'.$name.'</div>';
   echo '<div class="theme"><img title="'.$name.'" onclick="opentheme(this.title)" class="themeImage" src="' . $array['ThemePreview'] . '"/><span class="themeName">'.$name.'</span></div>';
-}
+}*/
 echo "</div>";
 
 ?>
