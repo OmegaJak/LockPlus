f71969cb316dd2f586325cfa04a8b179673a5ebb
JunesiPhone <street.visions@yahoo.com>
2015-08-08 14:21:44 -0500

    Added pages to the bottom of themes

---
+++ php/theme1.php
@@ -0,0 +1,149 @@
+<?php
+require_once "pages/Paginated.php";
+require_once "pages/DoubleBarLayout.php";
+?>
+
+<?php
+error_reporting(0);
+echo '<meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '<link rel="stylesheet" href="preview.css" type="text/css">';
+echo '<script>function viewtheme(d){window.location.href = "http://lockplus.us/preview?" + d;}</script>';
+echo '<style>.nav{position: absolute;text-align: center;top: 10px;margin: 0 auto;width: 100%;}</style>';
+
+$dir    = 'themes';
+$list = glob("$dir/*.plist");
+function mtimecmp($a, $b) {
+        $mt_a = filemtime($a);
+        $mt_b = filemtime($b);
+        if ($mt_a == $mt_b)
+            return 0;
+        else if ($mt_a < $mt_b)
+            return -1;
+        else
+            return 1;
+    }
+usort($list, "mtimecmp");
+$reversed = array_reverse($list);
+
+$page = $_GET['page'];
+$pagedResults = new Paginated($reversed, 5, $page);
+
+while($row = $pagedResults->fetchPagedRow()) {  //when $row is false loop terminates
+    $path = $row;
+    $name = basename($row,'.plist');
+    $plistDocument = new DOMDocument();
+    $plistDocument->load($path);
+    $array = parsePlist($plistDocument);
+    echo '<div class="theme"><img title="'.$name.'" onclick="viewtheme(this.title)" class="themeImage" src="' . $array['ThemePreview'] . '"/><span class="themeName">'.$name.'</span></div>';
+  }
+
+  $pagedResults->setLayout(new DoubleBarLayout());
+  echo $pagedResults->fetchPagedNavigation();
+
+/*foreach ($reversed as $filename) {
+	$path = $filename;
+	$name = basename($filename,'.plist');
+	$plistDocument = new DOMDocument();
+	$plistDocument->load($path);
+	$array = parsePlist($plistDocument);
+  echo '<div class="theme"><img title="'.$name.'" onclick="viewtheme(this.title)" class="themeImage" src="' . $array['ThemePreview'] . '"/><span class="themeName">'.$name.'</span></div>';
+}*/
+
+
+
+
+function parseValue( $valueNode ) {
+  $valueType = $valueNode->nodeName;
+
+  $transformerName = "parse_$valueType";
+
+  if ( is_callable($transformerName) ) {
+    // there is a transformer function for this node type
+    return call_user_func($transformerName, $valueNode);
+  }
+
+  // if no transformer was found
+  return null;
+}
+function parse_integer( $integerNode ) {
+  return $integerNode->textContent;
+}
+
+function parse_string( $stringNode ) {
+  return $stringNode->textContent;
+}
+
+function parse_date( $dateNode ) {
+  return $dateNode->textContent;
+}
+
+function parse_true( $trueNode ) {
+  return true;
+}
+
+function parse_false( $trueNode ) {
+  return false;
+}
+function parse_dict( $dictNode ) {
+  $dict = array();
+
+  // for each child of this node
+  for (
+    $node = $dictNode->firstChild;
+    $node != null;
+    $node = $node->nextSibling
+  ) {
+    if ( $node->nodeName == "key" ) {
+      $key = $node->textContent;
+
+      $valueNode = $node->nextSibling;
+
+      // skip text nodes
+      while ( $valueNode->nodeType == XML_TEXT_NODE ) {
+        $valueNode = $valueNode->nextSibling;
+      }
+
+      // recursively parse the children
+      $value = parseValue($valueNode);
+
+      $dict[$key] = $value;
+    }
+  }
+
+  return $dict;
+}
+
+function parse_array( $arrayNode ) {
+  $array = array();
+
+  for (
+    $node = $arrayNode->firstChild;
+    $node != null;
+    $node = $node->nextSibling
+  ) {
+    if ( $node->nodeType == XML_ELEMENT_NODE ) {
+      array_push($array, parseValue($node));
+    }
+  }
+
+  return $array;
+}
+
+
+$plistDocument = new DOMDocument();
+$plistDocument->load($path);
+
+function parsePlist( $document ) {
+  $plistNode = $document->documentElement;
+
+  $root = $plistNode->firstChild;
+
+  // skip any text nodes before the first value node
+  while ( $root->nodeName == "#text" ) {
+    $root = $root->nextSibling;
+  }
+
+  return parseValue($root);
+}
+
+?>
