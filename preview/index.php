f6ba72d703edb85063b6aa5c3d7f8b9933dd178f
JunesiPhone <street.visions@yahoo.com>
2015-08-09 14:41:26 -0500

    Cleaned up preview page

--- preview/index.php
+++ preview/index.php
@@ -7,7 +7,8 @@
   <?php include("parse.php");?>
 </head>
 <body>
-  <?php echo '<img title="'.$name.'"  class="bgImage" src="' . $array['ThemePreview'] . '"/>' ?>
+
+  <?php echo '<div title="'.$name.'"  class="bgImage" style="background-image:linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(' . $array['ThemePreview'] . ');"></div>' ?>
 <div class="preview">
   <div class="imgpreview">
     <img class="bg" src="phone.png" width="290"/>
@@ -21,7 +22,7 @@
     <?php echo '<div id="size">Filesize: '. formatSizeUnits(filesize(str_replace("%20"," ",$path))) . '</div>'?>
   </div>
   <a href="http://lockplus.us"><div class='btn'>Create your own</div></a>
-  <a href="http://lockplus.info"><div class='btn'>Learn about LockPlus</div></a>
+  <a href="http://lockplus.info"><div class='btn'>Download LockPlus</div></a>
   <?php echo '<a href="https://twitter.com/intent/tweet?url=http%3A%2F%2FLockPlus.us/preview?'.urlencode($name).'&text=Check%20out%20this%20theme%20for%20LockPlus.&hashtags=LockPlus"><div class="btn">Share Theme</div></a>' ?>
 
 
