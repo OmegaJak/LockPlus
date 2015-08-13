f5d563d1dbd9b62955db3da4bd99c33a811f5e3d
JunesiPhone <street.visions@yahoo.com>
2015-08-13 16:40:16 -0500

    Tweet preview photo

--- preview/index.php
+++ preview/index.php
@@ -16,14 +16,22 @@
     </div>
   </div>
   <div class="creator">
-     <span style="font-size:10px;"><span style="position:relative;font-size:20px;top:4px;">&#9888;</span> Gradients do not render on text</span></br></br>
+     <!--<span style="font-size:10px;"><span style="position:relative;font-size:20px;top:4px;">&#9888;</span> Gradients do not render on text</span></br></br>-->
     <?php echo '<div id="name">Creator: <a style="text-decoration:underline;" href="http://lockplus.us/themer/themer.php?name='.$array["DevName"].'">'. $array['DevName'] . '</a></div>'?>
     <?php echo '<div id="theme">Theme: '. str_replace("%20"," ",$name) . '</div>'?>
     <?php echo '<div id="size">Filesize: '. formatSizeUnits(filesize(str_replace("%20"," ",$path))) . '</div>'?>
   </div>
   <a href="http://lockplus.us"><div class='btn'>Create your own</div></a>
   <a href="http://lockplus.info"><div class='btn'>Download LockPlus</div></a>
-  <?php echo '<a href="https://twitter.com/intent/tweet?url=http%3A%2F%2FLockPlus.us/preview?'.urlencode($name).'&text=Check%20out%20this%20theme%20for%20LockPlus.&hashtags=LockPlus"><div class="btn">Share Theme</div></a>' ?>
+  <?php
+
+if(file_exists('../php/themepreview/'.urlencode($name).'.jpg')){
+    echo '<a href="https://twitter.com/intent/tweet?url=http%3A%2F%2FLockPlus.us/preview?'.urlencode($name).'&text=Check%20out%20this%20theme%20'.str_replace("%20"," ",$name).'%20for%20LockPlus.%20http://LockPlus.us/php/themepreview/'.urlencode($name).'.jpg&hashtags=LockPlus"><div class="btn">Share Theme</div></a>';
+}else{
+  echo '<a href="https://twitter.com/intent/tweet?url=http%3A%2F%2FLockPlus.us/preview?'.urlencode($name).'&text=Check%20out%20this%20theme%20'.str_replace("%20"," ",$name).'%20for%20LockPlus.&hashtags=LockPlus"><div class="btn">Share Theme</div></a>';
+}
+
+  ?>
 
 
   <footer></footer>
