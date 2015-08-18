1183c05beb70db8429660c40779166bee5a09687
JunesiPhone <street.visions@yahoo.com>
2015-08-18 03:25:59 -0500

    Fixed popular list
    again

--- InfoSite/themes/popular/index.php
+++ InfoSite/themes/popular/index.php
@@ -18,13 +18,13 @@ error_reporting(0);
 $array = unserialize(file_get_contents('http://LockPlus.us/php/count/dlcount.bin'));
 
 foreach ($array as $key => $value) {
-    $name = basename($key,'.plist');
+    $name = basename($key,'.jpg');
     $count = $value;
     $exist = $key;
     if(file_exists('../'.$exist)){
         if($count > 20){
             $imgSrc = basename($key,'.plist');
-            echo '<div class="theme"><img title="'.$name.'" onclick="viewtheme(this.title)" class="themeImage" src="../themepreview/' . $imgSrc . '.jpg"/>
+            echo '<div class="theme"><img title="'.$name.'" onclick="viewtheme(this.title)" class="themeImage" src="../themepreview/' . $imgSrc . '"/>
             <span class="themeName">'.$name.'</span>
             <span class="themeName">Est Downloads: '.$count.'</span>
             </div>';
