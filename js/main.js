550bd42b292ef059fe880cbc2d289ce2771d12e5
OmegaJak <jak.kruger@gmail.com>
2015-08-20 17:54:55 -0500

    Added backspace key also deleting elements

--- js/main.js
+++ js/main.js
@@ -2001,6 +2001,12 @@ $(document).on('keydown', function(event) {
             case 46: //Delete key
                 action.removeFromScreen(action.selectedItem, true);
                 break;
+            case 8: //Backspace key
+                if (action.selectedItem.substring(0, 4) != 'text') {
+                    action.removeFromScreen(action.selectedItem, true);
+                    event.preventDefault();
+                }
+                break;
         }
     }
 
