9cc2c2af37e2204e9f84ef7d555fedb8eb924d01
JunesiPhone <street.visions@yahoo.com>
2016-08-01 18:00:13 -0500

    Fixed Mobile build

---
+++ mobileBuild/Build/mergeALL.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "************ Merging CSS ****************"
+
+sh ./mergeCSS.sh
+
+echo "************ Compress CSS ****************"
+
+sh ./compressCSS.sh main ../WebReady/main.css
+
+echo "************ Merging/Compress JS ****************"
+
+sh ./mergeJS.sh ../js/arrays.js ../js/app.js ../js/constants.js ../js/main.js ../js/widget.js ../js/toolpanel.js ../js/powerange.js ../js/editmenu.js ../js/elementmenu.js ../WebReady/mobilemain.js
+
+
