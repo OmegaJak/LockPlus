21cfa8700c2b194f960f2f11afe0acfbaabc81a4
JunesiPhone <street.visions@yahoo.com>
2016-12-14 09:14:21 -0600

    Better build process for mobile

--- mobileBuild/Build/mergeALL.sh
+++ mobileBuild/Build/mergeALL.sh
@@ -6,10 +6,12 @@ sh ./mergeCSS.sh
 
 echo "************ Compress CSS ****************"
 
-sh ./compressCSS.sh main ../WebReady/main.css
+sh ./compressCSS.sh main ../../mobile/css/main.css
+
+echo "************ Merging/Compress JS Includes ****************"
+
+sh ./mergeJS.sh ../../js/jquery-2.1.4.min.js ../../js/slick.min.js ../../js/jquery.mousewheel.min.js ../../js/jquery-ui.js ../../js/spectrum.js ../../js/prefixfree.min.js ../../js/StackBlur.js ../../mobile/includes.js
 
 echo "************ Merging/Compress JS ****************"
 
-sh ./mergeJS.sh ../js/arrays.js ../js/app.js ../js/constants.js ../js/main.js ../js/widget.js ../js/toolpanel.js ../js/powerange.js ../js/editmenu.js ../js/elementmenu.js ../WebReady/mobilemain.js
-
-
+sh ./mergeJS.sh ../../js/arrays.js ../../mobile/compiled/js/app.js ../../mobile/compiled/js/constants.js ../../mobile/compiled/js/main.js ../../mobile/compiled/js/widget.js ../../mobile/compiled/js/toolpanel.js ../../mobile/compiled/js/powerange.js ../../mobile/compiled/js/editmenu.js ../../mobile/compiled/js/elementmenu.js ../../mobile/mobilemain.js
--- mobileBuild/Build/mergeCss.sh
+++ mobileBuild/Build/mergeCss.sh
@@ -1,16 +1,16 @@
 echo "Deleting main.css"
 cd "`dirname "$0"`"
-rm ../WebReady/main.css
-rm ../WebReady/main.min.css
-rm ../WebReady/mobilemain.js
+rm ../../mobile/css/main.css
+rm ../../mobile/css/main.min.css
+rm ../../mobile/mobilemain.js
 
 echo "Creating file list of all sub css files"
 
-find .. -name '*.css' > cssfilelist.txt
+find ../../mobile/compiled/css -name '*.css' > cssfilelist.txt
 cat cssfilelist.txt
 
 echo "Concat all of files into new main.css"
 while read line
 do
-cat $line >> ../WebReady/main.css
+cat $line >> ../../mobile/css/main.css
 done < cssfilelist.txt
